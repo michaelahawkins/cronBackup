d5588d9432b1a43e19e12473d0ce905048e5fce7
Michael Hawkins <hawkmauk@gmail.com>
2017-05-03 18:42:47 +0100

    Initial bash script with environment checks Will attempt to mount device if missing and create target directory Created seperate conf file for variables

---
+++ cronBackup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# bash script to backup files using rsync
+# from a $SOURCE_DIR to a $TARGET_DIR
+#
+
+DEFAULT_CONFIG=$XDG_CONFIG_HOME/cronBackup.conf
+
+CONFIG=$1
+
+logger -s "Initialising backup..."
+if [ "$CONFIG" == "" ]; then
+    CONFIG=$DEFAULT_CONFIG
+fi
+
+logger -s "...reading configuration file: $CONFIG"
+source $CONFIG
+
+logger -s "...checking target: $TARGET_DIR"
+if grep -qs "$TARGET_MOUNT" /proc/mounts; then
+    logger "...device mounted"
+else
+    logger -s "...attempting to mount device"
+    mount $TARGET_DEVICE $TARGET_MOUNT 
+    if [ ! $? == 0 ]; then
+        logger -s "...could not mount device!"
+        exit
+    fi
+fi
+
+
+if [ -d "$TARGET_DIR" ]; then
+    logger "...directory exists"
+else
+    logger -s "...attempting to create directory"
+    mkdir -p $TARGET_DIR
+    if [ ! $? == 0 ]; then
+        logger -s "...could not create directory!"
+	exit
+    fi
+fi
+
